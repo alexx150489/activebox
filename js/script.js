d5f188481024374051e76fb2421420f529c33e10
alexx150489 <nikolskiy150489@gmail.com>
2016-12-23 22:41:46 +0300

    added js modal window

--- js/script.js
+++ js/script.js
@@ -1,17 +1,88 @@
-var menu = $(".navbar__main-navigation");
-var menuTouchBtn = $(".navbar__touch-menu");
+$(function() {
 
-menuTouchBtn.click(function(e) {
-	e.preventDefault();
-	menu.fadeToggle();
-});
+  //  = ADAPTIVE MENU =
 
-// На ширине больше 760пх меню открыто всегда
-$(window).resize(function() {
-	var wid = $(window).width();
+  var menu = $(".navbar__main-navigation");
+  var menuTouchBtn = $(".navbar__touch-menu");
 
-	if (wid > 760 && menu.is(":hidden")) {
-		menu.removeAttr("style");
-	}
+  menuTouchBtn.click(function(e) {
+    e.preventDefault();
+    menu.fadeToggle();
+  });
+
+  // На ширине больше 760пх меню открыто всегда
+  $(window).resize(function() {
+    var wid = $(window).width();
+
+    if (wid > 760 && menu.is(":hidden")) {
+      menu.removeAttr("style");
+    }
+
+  });
+
+
+  // = SMOOTH SCROLL v 2.0 =
+
+  $("#js-nav a").on("click", function(e) {
+
+    e.preventDefault();
+
+    var currentBlock = $(this).attr("href"),
+      currentBlockOffset = $(currentBlock).offset().top;
+
+    $("html, body").animate({
+      scrollTop: currentBlockOffset - 85
+    }, 500);
+
+  });
+
+  // for fixed header!!!
+
+  var navH = $("#js-navbar-wrapper").innerHeight();
+
+
+  $(document).on("scroll", function() {
+
+    var documentScroll = $(this).scrollTop();
+
+    if (documentScroll > navH) {
+
+      $("#js-navbar-wrapper").addClass("js-fixed-navbar");
+      $("#js-navbar").css("border-bottom", "none");
+
+    } else {
+
+      $("#js-navbar-wrapper").removeClass("js-fixed-navbar");
+
+    }
+  });
+
+
+  // = MODAL WINDOW =
+
+
+  $("#js-show-modal").on("click", function(e) {
+
+    e.preventDefault();
+    $("#js-modal").fadeIn(500);
+    $("body").append("<div class='overlay' id='js-overlay'></div>").addClass("open-modal");
+
+  });
+
+
+  $("#js-modal-close").on("click", function(e) {
+
+    e.preventDefault();
+    $("#js-modal").fadeOut(100);
+    $("body").removeClass("open-modal");
+    $("#js-overlay").remove();
+
+  });
+
+  $("body").on("click", "#js-overlay", function() {
+    $("#js-modal").fadeOut(100);
+    $("body").removeClass("open-modal");
+    $("#js-overlay").remove();
+  });
 
 });
